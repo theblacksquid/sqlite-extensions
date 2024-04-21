32551d53172bff3ce8972119beb7e5db0252b63b
Aeron Paul Sioson <theblacksquid@protonmail.com>
2024-04-22 02:38:01 +0800

    forgot license statement on entrypoint

--- sqlite-extensions.scm
+++ sqlite-extensions.scm
@@ -1,3 +1,21 @@
+;;  sqlite-extensions/records.scm --- SQLite3 extensions for SRFI-9 Records
+
+;; Copyright (C) 2024 Aeron Paul Sioson <theblacksquid@protonmail.com>
+;; 
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see
+;; <https://www.gnu.org/licenses/>.
+
 (define-module (sqlite-extensions)
   #:use-module (sqlite-extensions base)
   #:use-module (sqlite-extensions records)
